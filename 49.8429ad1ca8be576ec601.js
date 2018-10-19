ec2ab39bdc8489e8dc8b383428a1d595ec2a821d
Maxime Tricoire <max.tricoire@gmail.com>
2018-10-19 18:06:57 +0200

    Updates

--- 49.8429ad1ca8be576ec601.js
+++
@@ -1,8 +0,0 @@
-(self.webpackJsonp=self.webpackJsonp||[]).push([[49],{288:
-/*!**************************************************************************************************************!*\
-  !*** /home/leiko/dev/datathings/greycat/js/node_modules/monaco-editor/esm/vs/basic-languages/azcli/azcli.js ***!
-  \**************************************************************************************************************/
-/*! exports provided: conf, language */
-/*! all exports used */
-/*! ModuleConcatenation bailout: Module is referenced from these modules with unsupported syntax: /home/leiko/dev/datathings/greycat/js/node_modules/monaco-editor/esm/vs/basic-languages/azcli/azcli.contribution.js (referenced with import()) */function(e,t,n){"use strict";n.r(t),n.d(t,"conf",function(){return s}),n.d(t,"language",function(){return o});var s={comments:{lineComment:"#"}},o={defaultToken:"keyword",ignoreCase:!0,tokenPostfix:".azcli",str:/[^#\s]/,tokenizer:{root:[{include:"@comment"},[/\s-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}],[/^-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}]],type:[{include:"@comment"},[/-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":"key.identifier"}}],[/@str+\s*/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}]],comment:[[/#.*$/,{cases:{"@eos":{token:"comment",next:"@popall"}}}]]}}}}]);
-//# sourceMappingURL=49.8429ad1ca8be576ec601.js.map
